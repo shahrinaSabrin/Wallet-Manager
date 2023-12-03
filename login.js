46c66c6d3084368302c1c7ad70d0d38caad66caf
Jannat <shahrinasabrin@gmail.com>
2023-12-03 23:30:44 +0600

    updated

--- login.js
+++ login.js
@@ -1,29 +1,69 @@
-// console.log("login");
+
 document.addEventListener("DOMContentLoaded", function() {
   // Use the History API to navigate to another page without a full reload
     window.history.pushState({}, "", "./index.html");
     // Trigger the popstate event to ensure the browser updates the page
     window.dispatchEvent(new PopStateEvent("popstate"));
 });
-  document.getElementById("email").value = "jr.dev101@gmail.com";
-  document.getElementById("password").value = "123456#";
-const logInValidation = () => {
-  const validEmail = "jr.dev101@gmail.com";
-  const validPass = "123456#";
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+const updateWithdraw = () => {
+  const inputWithdraw = document.getElementById("withDrawMoney");
+  let currentBalanceBox = document.getElementById("balanceAmound").textContent;
+  let withdrawBox = document.getElementById("withdrawBox").textContent;
 
-  if (email === validEmail && password === validPass) {
-    // alert("login successful");
-
-    //navigate ISSUE!! See you later
-    // Use the History API to navigate to another page without a full reload
-    window.history.pushState({}, "", "./wallet.html");
-    // Trigger the popstate event to ensure the browser updates the page
-    window.dispatchEvent(new PopStateEvent("popstate"));
-  } else {
-    alert("login failed");
+  if (isNaN(parseInt(inputWithdraw.value)) || parseInt(inputWithdraw.value) > parseInt(currentBalanceBox)) {
+    alert("Sorry! You have insufficient balance");
+    return;
   }
+
+  const newBalance = parseInt(currentBalanceBox) - parseInt(inputWithdraw.value);
+  const newWithdraw = parseInt(withdrawBox) + parseInt(inputWithdraw.value);
+
+  // Set the New Status
+  document.getElementById("balanceAmound").textContent = newBalance;
+  document.getElementById("withdrawBox").textContent = newWithdraw;
+
+  // Clear the input field
+  inputWithdraw.value = "";
 };
-const loginBtn = document.getElementById("loginBtn");
-loginBtn.addEventListener("click", logInValidation);
+
+const updateDeposit = () => {
+  let inputDeposit = document.getElementById("depositMoney");
+  let currentBalanceBox = document.getElementById("balanceAmound").textContent;
+  let depositBox = document.getElementById("depositBox").textContent;
+
+ 
+  // if (parseInt(inputDeposit.value) < 0) {
+  //   alert("Please enter a valid amount");
+  //   return;
+  // }
+ if (isNaN(parseInt(inputDeposit.value)) || parseInt(inputDeposit.value) < 0) {
+    alert("Please enter a valid amount");
+    return;
+  }
+
+  console.log("ok");
+
+  const newBalance = parseInt(inputDeposit.value) + parseInt(currentBalanceBox);
+  const newDeposit = parseInt(depositBox) + parseInt(inputDeposit.value);
+
+  // Set the New Status
+  document.getElementById("balanceAmound").textContent = newBalance;
+  document.getElementById("depositBox").textContent = newDeposit;
+
+  // Clear the input field
+  inputDeposit.value = "";
+};
+
+//get the buttons
+const depositBtn = document.getElementById("depositBtn");
+const withdrawBtn = document.getElementById("withDrawBtn");
+
+depositBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent default form submission behavior
+  updateDeposit();
+});
+
+withdrawBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent default form submission behavior
+  updateWithdraw();
+});
